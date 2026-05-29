a923d90f146e249d1461405ff986f83a9d4b8b74
Richard Gutierrez <richardg760@hotmail.com>
2026-05-29 16:47:35 -0700

    Initial react App commit

---
+++ react-app/src/routes/AppRouter.tsx
@@ -0,0 +1,45 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import MainLayout from "../layouts/MainLayout";
+import AboutPage from "../pages/AboutPage";
+import ContactPage from "../pages/ContactPage";
+import HomePage from "../pages/HomePage";
+import NotFoundPage from "../pages/NotFoundPage";
+import ServicesPage from "../pages/ServicesPage";
+import { ROUTE_HEROES } from "./heroConfig";
+
+const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+        handle: { hero: ROUTE_HEROES.home },
+      },
+      {
+        path: "about",
+        element: <AboutPage />,
+        handle: { hero: ROUTE_HEROES.about },
+      },
+      {
+        path: "services",
+        element: <ServicesPage />,
+        handle: { hero: ROUTE_HEROES.services },
+      },
+      {
+        path: "contact",
+        element: <ContactPage />,
+        handle: { hero: ROUTE_HEROES.contact },
+      },
+      {
+        path: "*",
+        element: <NotFoundPage />,
+      },
+    ],
+  },
+]);
+
+export default function AppRouter() {
+  return <RouterProvider router={router} />;
+}
